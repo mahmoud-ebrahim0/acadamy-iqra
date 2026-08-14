780bbf4bc784adc765e7b26ecd612e1c3fd09ecb
User <user@example.com>
2026-08-14 05:33:10 +0300

    Configure backend for Vercel Serverless

--- backend/server.js
+++ backend/server.js
@@ -57,6 +57,10 @@ const connectDB = async () => {
 
 connectDB(); // Run asynchronously
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+export default app;
